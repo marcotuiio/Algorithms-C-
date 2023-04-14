a9b54d124b7953a7a9126ba649fc6cfe8652cb2b
Marco Túlio Barros <96429308+marcotuiio@users.noreply.github.com>
2023-04-13 21:03:35 -0300

    Update L3x02Primos.c

--- Mat Básica/L3x02Primos.c
+++ Mat Básica/L3x02Primos.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-int main(){
-    int n, i, div;
-    int contador = 0;
+void main () {
+    int n = 100; // intervalo
 
-    printf("Defina o fim do intervalo [1, N] para calcular os primos: ");
-    scanf("%d", &n);
+    printf("\n Primos no intervalo 1 - %d:\n\n 2 3", n);
 
-        if(n == 1){                                         //um número primo tem apenas 2 divisores, 
-            printf("NAO existem primos nesse intervalo");   //e 1 possui somente um divisor, logo não é primo
-        }else if(n == 2){                                  
-            printf("2 e o unico primo nesse intervalo");    //o primeiro primo é o 2
-        }else{
-            for(i=2; i<=n; i++){
-                for(div=2; div<i; div++){
-                    if(i % div != 0){
-                        contador = contador + 1;     //caso o número não for divisível por div, contador 
-                    }                                //soma 1 para saber quantos números não dividem i
-                }
-                if(contador == (i-2)){            //para  o numero ser primo, não pode ser divisível por 
-                    printf("%d   ", i);           //nenhum número entre 1 e ele mesmo e é isso que o contador
-                }                                 //está fazendo
-                contador = 0;
+    int atual = 5;
+    int divisor = 2;
+    while (atual <= n) {
+        int check = 0; 
+        while (atual - 1 > divisor) {
+            int resto = atual % divisor;
+            if (resto == 0) {
+                check = 1;
+                break;
             }
-        }  
-
-    return 0;
+            divisor++;
+        }
+        if (check == 0) {
+            printf(" %d", atual);
+        }
+        divisor = 2;
+        atual++;
+    }
+    printf("\n");
 }
